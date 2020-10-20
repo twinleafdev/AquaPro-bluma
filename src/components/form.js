707db108e11ea2fde4f27ec449639dab9086a823
Mike <mikeh@ioprint.com>
2020-10-20 19:59:28 -0400

    form

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Form = () => (
     <div className="sidebar">
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" netlify>
 <div className="field">
   <label className="label">Name</label>
   <div className="control">
